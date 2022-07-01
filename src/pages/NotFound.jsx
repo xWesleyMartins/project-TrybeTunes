df7362554cdcc8774f0a20474bf6dcb1b853a1bb
xWesleyMartins <wesley_869@hotmail.com.br>
2022-07-01 17:57:13 -0300

    requisito 1

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class notFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">notFound</div>
+
+    );
+  }
+}
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">profileEdit</div>
+
+    );
+  }
+}
