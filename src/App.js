5c896c5941a963532eec3c14441020ea7ed2e0da
xWesleyMartins <wesley_869@hotmail.com.br>
2022-07-01 17:51:46 -0300

    completando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Favorites from './pages/Favorites';
+import notFound from './pages/NotFound';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ Login } />
+        <Route path="/Search" component={ Search } />
+        <Route path="/Album/:id" component={ Album } />
+        <Route path="/Profile" component={ Profile } />
+        <Route path="/Profile/edit" component={ ProfileEdit } />
+        <Route path="/Favorites" component={ Favorites } />
+        <Route path="*" component={ notFound } />
+      </BrowserRouter>
+    );
   }
 }
 
