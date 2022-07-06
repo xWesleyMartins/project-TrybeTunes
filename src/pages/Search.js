15629e46c080757277c742b29562a04ba3894713
xWesleyMartins <wesley_869@hotmail.com.br>
2022-07-06 18:52:51 -0300

    completando requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
 
 export default class Search extends Component {
+state = {
+  search: '',
+  changeOfSearchButton: true,
+}
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.handleEnableSearchButton);
+  };
+
+  handleEnableSearchButton = () => {
+    const { search } = this.state;
+    const minCharacters = 2;
+    if (search.length >= minCharacters) {
+      this.setState({
+        changeOfSearchButton: false,
+      });
+    } else {
+      this.setState({
+        changeOfSearchButton: true,
+      });
+    }
+  };
+
   render() {
+    const { search, changeOfSearchButton } = this.state;
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        Search
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="search"
+          onChange={ this.handleChange }
+          value={ search }
+          placeholder="Nome da banda ou artista"
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ changeOfSearchButton }
+          onClick={ this.handleEnableSearchButton }
+          name="Pesquisar"
+          value="Pesquisar"
+        >
+          Pesquisar
+        </button>
+      </div>
 
     );
   }
